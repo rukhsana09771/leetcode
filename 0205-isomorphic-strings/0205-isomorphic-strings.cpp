f9244d31b797dabaf250b08a063a7ea3ffac18d9
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2024-03-17 22:21:45 +0530

    Time: 6 ms (65.47%), Space: 8 MB (89.59%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length() != t.length()) return false;
+        vector<int> v(150, 1000);
+        for(int i=0; i<s.length(); i++){
+            int idx = (int)s[i];
+            if(v[idx]==1000) v[idx] = s[i] - t[i];
+            else if(v[idx]!=(s[i]-t[i])) return false;
+        }
+        
+        // Emptying the vector
+        for(int i=0; i<150; i++)
+            v[i] = 1000;
+        for(int i=0; i<s.length(); i++){
+            int idx = (int)t[i];
+            if(v[idx]==1000) v[idx] = t[i] - s[i];
+            else if(v[idx]!=(t[i]-s[i])) return false;
+        }
+        return true;
+    }
+};
