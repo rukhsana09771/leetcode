83add4e2ad1a03e863e65b50aea989b2ea4a7dda
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2024-03-18 16:17:00 +0530

    Time: 19 ms (37.55%), Space: 22.9 MB (5.12%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int n = arr.size();
+        int noz = 0;
+        vector<int> v;
+        
+        for(int i=0; i<n; i++){
+            if(arr[i]!=0) v.push_back(arr[i]);
+            else noz++;
+        }
+        
+        for(int i=1; i<=noz; i++){
+            v.push_back(0);
+        }
+        
+        for(int i=0; i<n; i++){
+            arr[i] = v[i];
+        }
+    }
+};
