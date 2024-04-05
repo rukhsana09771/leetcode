56540a7e5ce7d022dec7958777e0608fc79121e6
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2024-04-05 14:40:04 +0530

    Time: 80 ms (66.61%), Space: 62 MB (82.17%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int idx = -1;
+        for(int i=1; i<=n-2; i++){
+            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+                idx = i;
+                break;
+            }
+        }
+        return idx;
+    }
+};
