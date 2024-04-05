ebd7627556d6f87c54ebfb6df228f89ca618d77c
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2024-04-05 14:45:48 +0530

    Time: 86 ms (34.62%), Space: 62.1 MB (8.63%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int n = arr.size();
+        /* int n = arr.size();
         int idx = -1;
         for(int i=1; i<=n-2; i++){
             if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
                 idx = i;
                 break;
             }
+        }
+        return idx; */
+        
+        // Binary Search
+        int n = arr.size();
+        int low = 1;
+        int high = n-2;
+        int idx = -1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                idx = mid;
+                break;
+            }
+            else if(arr[mid]<arr[mid-1]) high = mid -1;
+            else low = mid + 1;
         }
         return idx;
     }
