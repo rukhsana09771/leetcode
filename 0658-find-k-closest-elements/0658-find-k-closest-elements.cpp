0e4690212a32b3212a7c79bcc1a1f66adb462692
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2024-04-05 20:56:40 +0530

    Time: 35 ms (43.43%), Space: 34.3 MB (60.66%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        // S.C = O(k) & T.C = O(logn + klogk)
+        int n = arr.size();
+        vector<int> ans(k);
+        if(x<arr[0]){
+            for(int i=0; i<k; i++){
+                ans[i] = arr[i];
+            }
+            return ans;
+        }
+        
+        if(x>arr[n-1]){
+            int i = n-1;
+            int j = k-1;
+            while(j>=0){
+                ans[j] = arr[i];
+                j--;
+                i--;
+            }
+            return ans;
+        }
+        
+        int low = 0;
+        int high = n-1;
+        int mid = -1;
+        int t = 0;  // representing index of ans
+        bool flag = false;  // if x is present in arr or not
+        while(low<=high){
+            mid = low + (high-low)/2;
+            if(arr[mid]==x){
+                flag = true;
+                ans[t] = arr[mid];
+                t++;
+                break;
+            }else if(arr[mid]>x) high = mid-1;
+            else low = mid+1;
+        }
+        
+        int lb = high;
+        int ub = low;
+        if(flag==true){
+            lb = mid-1;
+            ub = mid+1;
+        }
+        
+        while(t<k && lb>=0 && ub<=n-1){
+            int d1 = abs(x-arr[lb]);
+            int d2 = abs(x-arr[ub]);
+            if(d1<=d2){
+                ans[t] = arr[lb];
+                lb--;
+            }else{
+                ans[t] = arr[ub];
+                ub++;
+            }
+            t++;
+        }
+        
+        if(lb<0){
+            while(t<k){
+                ans[t] = arr[ub];
+                ub++;
+                t++;
+            }
+        }
+        
+        if(ub>n-1){
+            while(t<k){
+                ans[t] = arr[lb];
+                lb--;
+                t++;
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+        
+    }
+};
