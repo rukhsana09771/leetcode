20918d88068014d2689402bbff94d3cde6f80bab
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2024-05-05 13:20:43 +0530

    Time: 535 ms (22.90%), Space: 232.4 MB (49.53%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     int ans = 0;
-    void merge(vector<int>& a, int low, int mid, int high){
+    void merge(vector<int>& arr, int low, int mid, int high){
         int i = low, j = mid + 1;
         while(i<=mid && j<=high){
-            if((long long int)a[i]>(long long int)2*a[j]){
+            if((long long int)arr[i]>(long long int)2*arr[j]){
                 ans += mid - i + 1;
                 j++;
-                }
-            else i++;
             }
+            else i++;
+        }
         i = low, j = mid + 1;
         vector<int> b;
         while(i<=mid && j<=high){
-        if(a[i]<a[j]) b.push_back(a[i++]);
-        else b.push_back(a[j++]);
+        if(arr[i]<arr[j]) b.push_back(arr[i++]);
+        else b.push_back(arr[j++]);
     }
-    while(i<=mid) b.push_back(a[i++]);
-    while(j<=high) b.push_back(a[j++]);
-        for(int i=low; i<=high; i++) a[i] = b[i-low];
+    while(i<=mid) b.push_back(arr[i++]);
+    while(j<=high) b.push_back(arr[j++]);
+        for(int i=low; i<=high; i++) arr[i] = b[i-low];
         }
-    void mergeSort(vector<int>& a, int low, int high){
+    void mergeSort(vector<int>& arr, int low, int high){
         if(low==high) return;
         int mid = low + (high-low)/2;
-        mergeSort(a, low, mid);
-        mergeSort(a, mid+1, high);
-        merge(a, low, mid, high);
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid+1, high);
+        merge(arr, low, mid, high);
         }
     int reversePairs(vector<int>& nums) {
         mergeSort(nums, 0, nums.size()-1);
