22936c68b039ef1040a7fb06195534927e1ed477
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2024-06-30 14:13:58 +0530

    Time: 152 ms (86.27%), Space: 116.4 MB (92.86%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,13 +1,4 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 ListNode* reverseList(ListNode* head) {
        ListNode* prev = NULL, *curr = head, *Next = head;
         while(curr!=NULL){
@@ -21,24 +12,19 @@ ListNode* reverseList(ListNode* head) {
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        ListNode* c = new ListNode(100);
-        // deep copy of head
-        ListNode* temp = head;
-        ListNode* tempC = c;
-        while(temp){
-            ListNode* node = new ListNode(temp->val);
-            tempC->next = node;
-            temp = temp->next;
-            tempC = tempC->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        c = c->next;
-        c = reverseList(c);
+        ListNode* Node = reverseList(slow->next);
         ListNode* a = head;
-        ListNode* b = c;
-        while(a){
+        ListNode* b = Node;
+        while(b){
             if(a->val != b->val) return false;
             a = a->next;
-            b = b->next;
+            b =  b->next;
         }
         return true;
     }
