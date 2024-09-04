40613354cba6ec2f6d046682a1f5d2a0bfc73387
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2024-09-04 13:52:28 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (68.86%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,3 +1,52 @@
+// class MyQueue {
+// public:
+//     stack<int> st;
+//     stack<int> healper;
+//     MyQueue() {
+        
+//     }
+    
+//     void push(int x) { // O(1)
+//         st.push(x);
+//     }
+    
+//     int pop() {  // O(n)
+//         // remove at bottom
+//         while(st.size()>0){
+//             healper.push(st.top());
+//             st.pop();
+//         }
+//         int x = healper.top();
+//         healper.pop();
+//         while(healper.size()>0){
+//             st.push(healper.top());
+//             healper.pop();
+//         }
+//         return x;
+//     }
+    
+//     int peek() { // front O(n)
+//         // retreive at bottom
+//         while(st.size()>0){
+//             healper.push(st.top());
+//             st.pop();
+//         }
+//         int x = healper.top();
+//         while(healper.size()>0){
+//             st.push(healper.top());
+//             healper.pop();
+//         }
+//         return x;
+//     }
+    
+//     bool empty() {
+//         if(st.size()==0) return true;
+//         else return false;
+//     }
+// };
+
+
+// 2nd approach
 class MyQueue {
 public:
     stack<int> st;
@@ -6,37 +55,29 @@ public:
         
     }
     
-    void push(int x) { // O(1)
+    void push(int x) { // O(n)
+        // push at bottom 
+        if(st.size()==0) st.push(x);
+        else{
+            while(st.size()>0){
+              healper.push(st.top());
+              st.pop();
+        }
         st.push(x);
-    }
-    
-    int pop() {  // O(n)
-        // remove at bottom
-        while(st.size()>0){
-            healper.push(st.top());
-            st.pop();
-        }
-        int x = healper.top();
-        healper.pop();
         while(healper.size()>0){
             st.push(healper.top());
             healper.pop();
         }
+    }
+ }
+    int pop() {  // O(1)
+        int x = st.top();
+        st.pop();
         return x;
     }
     
-    int peek() { // front O(n)
-        // retreive at bottom
-        while(st.size()>0){
-            healper.push(st.top());
-            st.pop();
-        }
-        int x = healper.top();
-        while(healper.size()>0){
-            st.push(healper.top());
-            healper.pop();
-        }
-        return x;
+    int peek() { // front O(1)
+        return st.top();
     }
     
     bool empty() {
