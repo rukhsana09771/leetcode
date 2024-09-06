1f4289abee1556e26d3a078b167286190ff0a8ad
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2024-09-06 17:14:38 +0530

    Time: 188 ms (31.65%), Space: 137.3 MB (80.92%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& arr, int k) {
         if(k==1) return arr;
@@ -26,6 +26,26 @@ public:
                 j = ngi[j];
             }
             ans.push_back(mx);
+        }
+        return ans;
+    }
+};  */
+
+// 2nd approach
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+        if(k==1) return arr;
+        int n = arr.size();
+        vector<int> ans; // i should be min k-1
+        deque<int> dq; // decreasing order of ele (index)
+        for(int i=0; i<n; i++){
+            while(dq.size()>0 && arr[i]>arr[dq.back()]) 
+            dq.pop_back();
+            dq.push_back(i);
+            int j = i-k+1; // start of the window
+            while(dq.front()<j) dq.pop_front();
+            if(i>=k-1) ans.push_back(arr[dq.front()]);
         }
         return ans;
     }
