0eee141d845ebefb9ead829f0b6a0139a2278bfb
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2025-05-02 08:20:42 +0530

    Time: 3 ms (91.56%), Space: 15.3 MB (30.42%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,8 +17,7 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        
-        // Create the deep copy without random pointer
+        // step 1 -> create the deep copy without random pointer
         Node* dummy = new Node(0);
         Node* tempC = dummy;
         Node* temp = head;
@@ -28,49 +27,28 @@ public:
             tempC = tempC->next;
             temp = temp->next;
         }
-        
-        //making alternate connection
-        Node* duplicate = dummy->next;
+        Node* b = dummy->next;
         Node* a = head;
-        Node* b = duplicate;
-        dummy = new Node(-1);
-        Node* tempD = dummy;
-        while(a){
-            tempD->next = a;
-            tempD = tempD->next;
-            a = a->next;
-            tempD->next = b;
-            tempD = tempD->next;
-            b = b->next;
+        // step 2 -> make a map of <original, duplicate>
+        unordered_map<Node*, Node*> m;
+        Node* tempa = a;
+        Node* tempb = b;
+        while(tempa!=NULL){
+            m[tempa] = tempb;
+            tempa = tempa->next;
+            tempb = tempb->next;
+
         }
-        
-        // Assigning random pointer
-        dummy = dummy->next;
-        Node* t1 = dummy; // t1 wil traverse in the original list
-        while(t1){
-            Node* t2 = t1->next;  // t2 is for duplicate
-            if(t1->random) t2->random = t1->random->next;
-            t1 = t1->next->next;
+        for(auto x : m){
+            Node* o = x.first;
+            Node* d = x.second;
+            if(o->random!=NULL){
+                Node* oRandom = o->random;
+                Node* dRandom = m[o->random];
+                d->random = dRandom;
+            }
         }
-        
-        // removing the connection(seperate)
-        Node* d1 = new Node(-1);
-        Node* d2 = new Node(-1);
-        t1 = d1;
-        Node* t2 = d2;
-        Node* t = dummy;
-        while(t){
-            t1->next = t;
-            t1 = t1->next;
-            t = t->next;
-            t2->next = t;
-            t2 = t2->next;
-            t = t->next;
-        }
-        t1->next = NULL;
-        t2->next = NULL;
-        d1 = d1->next;
-        d2 = d2->next;
-        return d2;
+        return b;
+
     }
 };
