44f28569ae964227f56303311de1d39cd07aff51
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2025-10-19 08:01:55 +0530

    Time: 8 ms (26.55%), Space: 24.7 MB (16.82%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,17 +1,17 @@
-// backward merge
+// forward  merge
 class Solution {
 public:
     static bool cmp(vector<int>& a, vector<int>& b){
-        return a[1]<b[1];
+        return a[0]<b[0];
     }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end(), cmp);
         vector<vector<int>> result;
         int n = intervals.size();
-        result.push_back(intervals[n-1]);
-        for(int i=n-2; i>=0; i--){
+        result.push_back(intervals[0]);
+        for(int i=1; i<n; i++){
             vector<int> curr = intervals[i];
-            if(curr[1]>=result[result.size()-1][0]){
+            if(curr[0]<=result[result.size()-1][1]){
                 result[result.size()-1][0] = min(result[result.size()-1][0], curr[0]);
                 result[result.size()-1][1] = max(result[result.size()-1][1], curr[1]);
             }else{
@@ -19,7 +19,6 @@ public:
             }
         
         }
-        reverse(result.begin(), result.end());
         return result;
     }
 };
