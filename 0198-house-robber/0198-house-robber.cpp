ec4e66508681d91ee17eda52057deb7bba00e7f8
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2025-11-04 08:39:48 +0530

    Time: 0 ms (100%), Space: 10.6 MB (64.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     vector<int> dp;
-    int ftd(vector<int> arr, int i){
-        if(i==arr.size()-1) return arr[i];
-        if(i==arr.size()-2) return max(arr[i], arr[i+1]);
-        if(dp[i]!=-1) return dp[i];
-        return dp[i] = max(arr[i]+ftd(arr, i+2), 0+ftd(arr, i+1));
+    int fbu(vector<int> arr){
+        int n = arr.size();
+        if(n==1) return arr[0];
+        dp.clear();
+        dp.resize(n);
+        dp[n-1] = arr[n-1];
+        dp[n-2] = max(arr[n-1], arr[n-2]);
+        for(int i=n-3; i>=0; i--){
+            dp[i] = max(arr[i]+dp[i+2], dp[i+1]);
+        }
+        return dp[0];
     }
     int rob(vector<int>& nums) {
-        dp.clear();
-        dp.resize(104, -1);
-        return ftd(nums, 0);
+        return fbu(nums);
     }
 };
