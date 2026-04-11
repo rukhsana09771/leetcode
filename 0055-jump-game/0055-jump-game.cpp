20d90065a60e9869c1b90d40dee51ef350033a0d
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2026-04-11 11:18:19 +0530

    Time: 0 ms (100%), Space: 52.2 MB (82.12%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int maxInd = 0;
+        for(int i=0; i<n; i++){
+            if(i>maxInd) return false;
+            maxInd = max(maxInd, i+nums[i]);
+            if(maxInd>=n-1) return true;
+        }
+        return true;
+    }
+};
